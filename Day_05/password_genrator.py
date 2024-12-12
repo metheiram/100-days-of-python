62a6a611de3a8b14298ca3159c94d0cc834b2fa5
metheiram <iamhinamuhtar@gmaol.com>
2024-12-11 20:13:34 -0800

    message here

--- Day_05/password_genrator.py
+++ Day_05/password_genrator.py
@@ -1,5 +1,5 @@
 #random password generator 
-
+import random
 letter=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 number=['1', '2',' 3', '4', '5', '6', '7', '8',' 9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
@@ -10,9 +10,16 @@ r_letter=int(input("how many letters you want to include"))
 r_number=int(input("how many numbers you want to include"))
 r_symbols=int(input("how many symbols you want to include"))
 
-
+password = []
 #easy level you can generate it by sequence 
 
-generate = r_letter,r_number,r_symbols
-print(generate)
+for lett in range(1,r_letter+1):
+    password += random.choice(letter)
+for no in range (1 , r_number+1):
+    password += random.choice(number)
 
+for sy in range (1 , r_symbols+1):
+    password += random.choice(symbols)
+
+random.shuffle(password)
+print(password)
--- Day_05/tempCodeRunnerFile.py
+++ Day_05/tempCodeRunnerFile.py
@@ -1,18 +1,25 @@
-#cheaking heighest score 
-
-students_score = [100,23,45,67,89,99,199,182,788,34,55,67]
-
-# totalmarks=sum(students_score)
-# print(totalmarks)
-
-# max_num  = max(students_score)
-# print(max_num)
+#random password generator 
+import random
+letter=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+number=['1', '2',' 3', '4', '5', '6', '7', '8',' 9']
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
 
 
-#now the task is  find maximum number by using loop 
-score=0
-for max_score in students_score:
-    if max_score> score:
-       score= max_score
+print("Welcome to the  py  password generator")
+r_letter=int(input("how many letters you want to include"))
+r_number=int(input("how many numbers you want to include"))
+r_symbols=int(input("how many symbols you want to include"))
 
-print(score)
+password = []
+#easy level you can generate it by sequence 
+
+for lett in range(1,r_letter+1):
+    password += random.choice(letter)
+for no in range (1 , r_number+1):
+    password += random.choice(number)
+
+for sy in range (1 , r_symbols+1):
+    password += random.choice(symbols)
+
+random.shuffle(password)
+print(password)
